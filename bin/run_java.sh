d1703a94e35012db7309cea791afc1441946a99a
gubi <gubi.ale@iod.io>
2018-08-14 15:28:58 +0200

    run_java.sh doesn't recognize Java versions

--- bin/run_java.sh
+++ bin/run_java.sh
@@ -9,7 +9,7 @@ readonly SCRIPT_NAME="$1"
 shift
 
 readonly VERSION=$(java -version 2>&1 \
-    | sed -n '1s/.*version.*1\.\([0-9]\).*/\1/p')
+    | sed -n '1s/.*version \"\([0-9]\+\).*\".*/\1/p')
 case "${VERSION}" in
 [1-6])
   cat >&2 <<EOF
@@ -22,10 +22,10 @@ You can download the latest JDK from:
   http://www.oracle.com/technetwork/java/javase/downloads/index.html
 EOF
   exit 1;;
-
 [7-9])
   ;;
-
+1[0-9])
+  ;;
 *)
   echo "Warning: unable to determine Java version" >&2;;
 esac
